abe0307f8b09a8c792cfac52750c36f157a590e4
Brandon <kick361@hotmail.com>
2023-04-05 21:50:38 -0600

    Curso JS

--- JavaScript/Formulario/Formularios.js
+++ JavaScript/Formulario/Formularios.js
@@ -12,7 +12,29 @@ window.addEventListener("load", function () {
 
     var nombre = document.querySelector("#nombre").value;
     var apellidos = document.querySelector("#apellidos").value;
-    var edad = document.querySelector("#edad").value;
+    var edad = parseInt(document.querySelector("#edad").value);
+    if (nombre.trim() == null || nombre.trim().length == 0) {
+      alert("El nombre no es valido");
+      document.querySelector("#error_nombre").innerHTML = "El nombre no es valido";
+      
+      return false;
+    }else{
+      document.querySelector("#error_nombre").style.display="none";
+      document.querySelector("#nombre").style.color = "red";
+
+
+    }
+
+
+    
+    if (apellidos.trim() == null || apellidos.trim().length == 0) {
+      alert("El apellido no es valido");
+      return false;
+    }
+    if (edad == null || edad <= 0 || isNaN(edad)) {
+      alert("La edad no es valido");
+      return false;
+    }
 
     box_dashed.style.display = "block";
 
@@ -20,15 +42,15 @@ window.addEventListener("load", function () {
     console.log(nombre, apellidos, edad);
 
 
-var datos_usuarios = [nombre,apellidos,edad];
-var indice;
-for(indice in datos_usuarios){
-    var parrafo = document.createElement("p");
-    parrafo.append(datos_usuarios[indice]);
-    box_dashed.append(parrafo);
-    
+    var datos_usuarios = [nombre, apellidos, edad];
+    var indice;
+    for (indice in datos_usuarios) {
+      var parrafo = document.createElement("p");
+      parrafo.append(datos_usuarios[indice]);
+      box_dashed.append(parrafo);
 
-}
+
+    }
 
 
   });
