85e44fdc71059e3b3307a4cc9cdcd37077398575
Brandon <kick361@hotmail.com>
2023-05-12 16:43:18 -0600

    Curso JS

---
+++ JavaScript/jQuery/Interface/Interface.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  //MOVER ELEMENTO POR LA PÁGINA
+  $(".elemento").draggable();
+
+  //REDIMENSIONAR
+  $(".elemento").resizable();
+
+  //SELECCONAR ELEMENTOS
+  // $('.lista-seleccionable').selectable();
+
+  //ORDENAR ELEMENTOS
+  $(".lista-seleccionable").sortable({
+    update: function (event, ui) {
+      console.log("Ha cambiado la lista");
+    },
+  });
+
+  //EFECTOS DE SOLTAR Y ARRASTRAR
+  $("elemento-movido").draggable();
+  $("#area").droppable({
+    drop: function () {
+      console.log("Has soltado algo");
+    },
+  });
+
+  //TOOLTIP
+  $(document).tooltip();
+
+  //DIALOGO
+  $("#lanzar-popup").click(function () {
+    $("#popup").dialog();
+  });
+
+  //DATEPICKER
+  $("#calendario").datepicker();
+
+  //TABS
+  
+});
