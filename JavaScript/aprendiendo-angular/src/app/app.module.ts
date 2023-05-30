cafab6ece784a57033d2dfd637e4c7d5e2d31f8f
Brandon <kick361@hotmail.com>
2023-05-29 18:04:08 -0600

    Curso Angular

---
+++ JavaScript/aprendiendo-angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { ZapatosComponent } from './zaptos/zapatos.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VideojuegoComponent,
+    ZapatosComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ JavaScript/aprendiendo-angular/src/app/zaptos/zapatos.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+    selector: 'zapatos',
+    templateUrl: './zapatos.component.html'
+})
+
+export class ZapatosComponent {
+
+        public titulo: string = "Componente de zapatos"
+
+}
