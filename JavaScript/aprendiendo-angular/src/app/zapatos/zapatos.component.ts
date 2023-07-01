f342b51a35856d58b40cfe1682e42e9d825293c4
Brandon <kick361@hotmail.com>
2023-06-30 18:08:06 -0600

    JS

--- JavaScript/aprendiendo-angular/src/app/zapatos/zapatos.component.ts
+++ JavaScript/aprendiendo-angular/src/app/zapatos/zapatos.component.ts
@@ -9,33 +9,46 @@ export class ZapatosComponent implements OnInit {
   public titulo: string = 'Componente de zapatos';
   public zapatos: Array<zapato>;
   public marcas: string[];
+  public color: string;
+  public mi_marca:string;
   constructor() {
-    this.marcas = new Array;
+    this.mi_marca = 'Jordan'
+    this.color = 'orange';
+    this.marcas = new Array();
     this.zapatos = [
-        new zapato('Nike classic', 'Nike', 'Negro', 90, true),
-        new zapato('Air Jordan', 'Nike', 'Rojo', 80, false),
-        new zapato('Rebook Delux', 'Rebook', 'Amarillo',120, true),
-        new zapato('Gucci', 'Adidas', 'Blanco',40, true),
-        new zapato('Converse', 'Rebook', 'Amarillo',70, false ),
-
-
+      new zapato('Nike classic', 'Nike', 'Negro', 90, true),
+      new zapato('Air Jordan', 'Nike', 'Rojo', 80, false),
+      new zapato('Rebook Delux', 'Rebook', 'Amarillo', 120, true),
+      new zapato('Gucci', 'Adidas', 'Blanco', 40, true),
+      new zapato('Converse', 'Rebook', 'Amarillo', 70, false),
     ];
   }
-  ngOnInit(){
+  ngOnInit() {
     console.log(this.zapatos);
 
     this.getMarcas();
   }
 
-  getMarcas(){
-    this.zapatos.forEach((zapato,index)=>{
-      if(this.marcas.indexOf(zapato.marca)<0){
+  getMarcas() {
+    this.zapatos.forEach((zapato, index) => {
+      if (this.marcas.indexOf(zapato.marca) < 0) {
         this.marcas.push(zapato.marca);
-      // console.log(index);
+        // console.log(index);
       }
-    
     });
     console.log(this.marcas);
   }
+
+  getMarca(){
+    alert(this.mi_marca);
+  }
+
+  addMarca(){
+    this.marcas.push(this.mi_marca);
+  }
+  onBlur(){
+    console.log("Has salido del input");
+  }
+  
 
 }
